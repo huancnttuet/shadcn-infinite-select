cca662f92ad39499f77368df3c2bfc09a94f3069
huanpl <huanpl@vicoders.com>
2025-02-27 18:47:19 +0700

    update

--- src/configs/site.ts
+++ src/configs/site.ts
@@ -5,13 +5,13 @@ export const META_THEME_COLORS = {
 
 export const siteConfig = {
   name: 'shadcn/ui infinite extension',
-  url: 'https://ui.shadcn.com',
+  url: 'https://shadcn-infinite-select.vercel.app/',
   ogImage: 'https://ui.shadcn.com/og.jpg',
   description:
     'Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.',
   links: {
     twitter: 'https://twitter.com/shadcn',
-    github: 'https://github.com/shadcn-ui/ui',
+    github: 'https://github.com/huancnttuet/shadcn-infinite-select',
   },
 };
 
